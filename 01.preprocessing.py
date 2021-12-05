c832a3db57a8f4fa934b98cef8fb53b83186fd46
Nang <goldenrezgun@gmail.com>
2021-12-05 11:20:43 +0700

    Remove special character and filter stopwords

--- 01.preprocessing.py
+++ 01.preprocessing.py
@@ -1,26 +1,35 @@
 import json
+import re
+import nltk
+from nltk.corpus import stopwords
 
+stop_words = set(nltk.corpus.stopwords.words('english'))
 
 # f = open("yelp_academic_dataset_review.json", "r")
 f = open("dataset.json", "r")
 
 Lines = f.readlines()
 counts = dict()
-count = 0
-# Strips the newline character
-for line in Lines:
-    count += 1
-    if count > 10:
-        break
+
+
+for line in Lines:    
     j_data = json.loads(line)
-    print(j_data['text'])
+    # print(j_data['text'])
     sentence = j_data['text']
     words = sentence.split()
     for word in words:
-        
+        word = re.sub('[^a-zA-Z]+', '', word)
         if word in counts:
             counts[word] += 1
         else:
             counts[word] = 1
 
-print(count)
+
+filtered = dict()
+for word in counts:
+    if not word in stop_words:
+        filtered[word] = counts[word]
+
+
+print('Original', len(counts))
+print('Filtered', len(filtered))
