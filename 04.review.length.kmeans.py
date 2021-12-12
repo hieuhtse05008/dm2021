b34fbe75748fb3d430f4a4199164bc9f308642c7
Nang <goldenrezgun@gmail.com>
2021-12-12 11:35:21 +0700

    labwork_4

---
+++ 04.review.length.kmeans.py
@@ -0,0 +1,34 @@
+import random
+import utils
+from utils import Point, Cluster
+
+wc = utils.loadData()
+
+points = [Point(x) for x in wc]
+
+K = 3
+
+centroids = [random.choice(points) for i in range(0, K)]
+
+while True:
+    clusters = [Cluster() for i in range(0, K)]
+    for p in points:
+        distances = [p.dist(centroid) for centroid in centroids]
+        argMin = distances.index(min(distances))
+        clusters[argMin].addPoint(p)
+
+    print(clusters)
+    newCentroids = [Point(c.getCentroid()) for c in clusters]
+    print(f"Centroids: {[c.getCentroid for c in clusters]}")
+    centroidsDiffs = []
+    for c1, c2 in zip(centroids, newCentroids):
+        centroidsDiffs.append(c1.dist(c2))
+
+    sse = 0
+    for dif in centroidsDiffs:
+        sse += dif ** 2
+    centroids.clear()
+    centroids = newCentroids
+    if sse < 1:
+        print("Stop")
+        break
---
+++ utils.py
@@ -0,0 +1,57 @@
+import json
+import statistics
+
+
+def loadData():
+    documents = []
+    with open("./yelp_academic_dataset_review.json", "r") as f:
+        for i, text in enumerate(f):
+            if(i == 10):
+                break
+
+            jsonText = json.loads(text)
+            realText = jsonText['text']
+            documents.append(realText)
+
+    data = [len(doc.split()) for doc in documents]
+    return data
+
+
+class Point:
+    def __init__(self, x) -> None:
+        self.x = x
+
+    def dist(self, _p):
+        return abs(self.x - _p.x)
+
+
+class Cluster:
+    def __init__(self, policy=min):
+        self.points = []
+        self.policy = policy
+
+    def addPoint(self, p):
+        self.points.append(p)
+
+    def dist(self, _c):
+        d = 0
+        if(self.policy == min):
+            d = float('inf')
+        else:
+            d = -1
+        for p1 in self.points:
+            for p2 in _c.points:
+                d = self.policy(d, p1.dist(p2))
+        return d
+
+    def merge(self, _c):
+        self.points += _c.points
+
+    def __repr__(self):
+        return f"{[p.x for p in self.points]}"
+    def getCentroid(self):
+        return self.policy([p.x for p in self.points])
+    
+
+def getDist(p):
+    return p['dist']
